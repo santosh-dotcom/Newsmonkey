0a7bbfdcb271c72b2230a6b8d5924f541133bf89
santoshdeo <semtitans.santosh@gmail.com>
2023-08-21 08:20:39 +0530

    Hide apikey

--- src/components/News.js
+++ src/components/News.js
@@ -1,40 +1,141 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 
+  static defaultProps = {
+    country: 'in',
+    pagesize: 8,
+    category: 'general',
+  }
+
+  static protoTypes = {
+    country: PropTypes.string,
+    pagesize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
   // articles= [{"source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com","description":"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg","publishedAt":"2020-04-27T11:41:47Z","content":"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"},{"source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com","description":"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg","publishedAt":"2020-03-30T15:26:05Z","content":"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"}]
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     console.log("Hello i am a constructor from news component");
     this.state ={
       articles: [],
-      loading: false
+      loading: true,
+      page: 1 ,
+      totalResults: 0    
     }    
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`
   }
 
+async updateNews(){
+  this.props.setProgress(0);
+  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
+  this.setState({loading: true})
+  let data = await fetch(url);
+  let parseData = await data.json()
+  console.log(parseData);
+  this.setState({articles: parseData.articles, totalResults: parseData.totalResults, loading: false})
+  this.props.setProgress(100);
+}
+
+
+
  async componentDidMount(){
-    console.log('cdm');
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d738c6652e08427dbb2c1b9dfe7347a4";
-    let data = await fetch(url);
-    let parseData = await data.json()
-    console.log(parseData);
-    this.setState({articles: parseData.articles})
+    // console.log('cdm');
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d738c6652e08427dbb2c1b9dfe7347a4&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true})
+    // let data = await fetch(url);
+    // let parseData = await data.json()
+    // console.log(parseData);
+    // this.setState({articles: parseData.articles, totalResults: parseData.totalResults, loading: false})
+
+    this.updateNews();
   }
+
+  handleNextClick = async() =>{
+    // if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){     
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d738c6652e08427dbb2c1b9dfe7347a4&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true})
+    // let data = await fetch(url);
+    // let parseData = await data.json()
+    // console.log(parseData);
+    
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parseData.articles,
+    //     loading: false
+    //   })
+    // }
+    
+    this.setState({page: this.state.page + 1});
+    this.updateNews();
+  }
+
+  handlePrevClik = async() =>{
+
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d738c6652e08427dbb2c1b9dfe7347a4&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading: true})
+    // let data = await fetch(url);
+    // let parseData = await data.json()
+    // console.log(parseData);
+    
+    //   this.setState({
+    //     page: this.state.page - 1,
+    //     articles: parseData.articles,
+    //     loading: false
+    //   })
+
+    this.setState({page: this.state.page - 1});
+    this.updateNews();
+  }
+
+
+  fetchMoreData = async () => {   
+    this.setState({page: this.state.page + 1})
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
+  // this.setState({loading: true})
+  let data = await fetch(url);
+  let parseData = await data.json()
+  console.log(parseData);
+  this.setState({articles: this.state.articles.concat(parseData.articles), totalResults: parseData.totalResults})
+  };
 
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewMonkey - Top Headlines</h1> 
+        <h1 className='text-center' style={{ margin: "35px 0" }}>NewMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1> 
+       {/* {this.state.loading && <Spinner/>} */}
+
+       <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div className="container">
         <div className="row">  
         {this.state.articles.map((element) => {
           return <div className="col-md-4" key={element.url}>
-          <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 86):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 86):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
           </div>         
-        })}
+        })}       
           
         </div> 
+        </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePrevClik}> &larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div> */}
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,19 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
 
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
+          <div>
+        <span className="display-flex badge rounded-pill bg-danger" style={{right: '0', zindex:"1" }}> {source}
+            </span></div>
           <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/08/15/1600x900/bgmi_1688952381443_1692063446647.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="-blank" className="btn btn-sm btn-primary">
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <a rel="noreferrer" href={newsUrl} target="-blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
